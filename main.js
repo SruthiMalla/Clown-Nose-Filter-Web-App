95039feb11ec066f57bc9a7df8150c739329c0c1
SruthiMalla <52434141+SruthiMalla@users.noreply.github.com>
2022-06-22 19:32:25 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+nose_x = 0;
+nose_y = 0;
+
+function preload()
+{
+    clown_nose = loadImage("Clown Nose.png");
+}
+
+function setup()
+{
+    canvas = createCanvas(300,300);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw()
+{
+    image(video, 0, 0, 300, 300);
+    image(clown_nose, nose_x, nose_y, 30, 30);
+}
+
+function take_snapshot()
+{
+    save("myfilter.png");
+}
+
+function modelLoaded()
+{
+    console.log("PoseNet is Initialized.");
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+
+        console.log("nose_x = " + results[0].pose.nose.x);
+        console.log("nose_y = " + results[0].pose.nose.y);
+
+        nose_x = results[0].pose.nose.x-15;
+        nose_y = results[0].pose.nose.y-15;
+    }
+}
